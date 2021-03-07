242a83d0e20670200a6d2de8fb5341af723f030d
Robert Cooper <robert.cooper.qu@gmail.com>
2021-03-07 15:45:17 -0500

    Nested folders

--- utils/mdxUtils.js
+++ utils/mdxUtils.js
@@ -7,5 +7,9 @@ export const POSTS_PATH = path.join(process.cwd(), 'posts')
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
 export const postFilePaths = fs
   .readdirSync(POSTS_PATH)
+  .flatMap(folder => {
+    const absolute = path.join(POSTS_PATH, folder);
+    return fs.readdirSync(absolute)
+  })
   // Only include md(x) files
-  .filter((path) => /\.mdx?$/.test(path))
+  // .filter((path) => /\.mdx?$/.test(path))
