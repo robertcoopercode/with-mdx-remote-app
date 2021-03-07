770bb70398b551f0b632748a4337aef844d834ef
Robert Cooper <robert.cooper.qu@gmail.com>
2021-03-07 13:45:33 -0500

    test

---
+++ pages/rss.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import { join } from 'path';
+import getConfig from 'next/config';
+const { serverRuntimeConfig } = getConfig();
+const postsDirectory = join(String(serverRuntimeConfig.PROJECT_ROOT), 'content', 'posts');
+
+const Page = () => null;
+
+export default Page;
+
+export const getServerSideProps = (context) => {
+  const res = context.res;
+  const posts = fs.readdirSync(postsDirectory);
+  console.log(posts);
+  return { props: {} };
+}
